3b7d51722e749d8a1ddf6829481805ad0000b910
noobmaster69jr <srashwin22@gmail.com>
2024-02-15 11:31:04 +0530

    added one curd api

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
-const app = express();
+const app = express(); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
 
