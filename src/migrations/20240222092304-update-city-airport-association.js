48c84fa025f900bc3db6bf793c556c15ccab46af
noobmaster69jr <srashwin22@gmail.com>
2024-02-22 15:14:42 +0530

    added city,airport models and relevant CRUD

---
+++ src/migrations/20240222092304-update-city-airport-association.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) { 
+    await queryInterface.addConstraint('Airports', {
+      type: 'FOREIGN KEY',
+      name: 'city_fkey_constraint',
+      fields: ['cityId'],
+      references: {
+        table: 'Cities',
+        field: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDELETE: 'CASCADE',
+    });
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('Airports', 'city_fkey_constraint');
+  },
+};
